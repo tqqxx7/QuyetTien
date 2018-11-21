cd5d0f615ae8ee7bee73f34e8ddcab30e80ad26f
US <tqqxx7@gmail.com>
2018-11-21 09:26:36 +0700

    add entity model

---
+++ QuyetTien/Controllers/CreateController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using QuyetTien.Models;
+
+namespace QuyetTien.Controllers
+{
+    public class CreateController : Controller
+    {
+        private CS4PEEntities db = new CS4PEEntities();
+
+        // GET: /Create/
+        public ActionResult Index()
+        {
+            var bangsanphams = db.BangSanPhams.Include(b => b.LoaiSanPham);
+            return View(bangsanphams.ToList());
+        }
+
+        // GET: /Create/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BangSanPham bangsanpham = db.BangSanPhams.Find(id);
+            if (bangsanpham == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bangsanpham);
+        }
+
+        // GET: /Create/Create
+        public ActionResult Create()
+        {
+            ViewBag.Loai_id = new SelectList(db.LoaiSanPhams, "id", "TenLoai");
+            return View();
+        }
+
+        // POST: /Create/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="id,MaSP,TenSP,Loai_id,GiaBan,GiaGoc,GiaGop,SoLuongTon")] BangSanPham bangsanpham)
+        {
+            if (ModelState.IsValid)
+            {
+                db.BangSanPhams.Add(bangsanpham);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Loai_id = new SelectList(db.LoaiSanPhams, "id", "TenLoai", bangsanpham.Loai_id);
+            return View(bangsanpham);
+        }
+
+        // GET: /Create/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BangSanPham bangsanpham = db.BangSanPhams.Find(id);
+            if (bangsanpham == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Loai_id = new SelectList(db.LoaiSanPhams, "id", "TenLoai", bangsanpham.Loai_id);
+            return View(bangsanpham);
+        }
+
+        // POST: /Create/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="id,MaSP,TenSP,Loai_id,GiaBan,GiaGoc,GiaGop,SoLuongTon")] BangSanPham bangsanpham)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(bangsanpham).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Loai_id = new SelectList(db.LoaiSanPhams, "id", "TenLoai", bangsanpham.Loai_id);
+            return View(bangsanpham);
+        }
+
+        // GET: /Create/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BangSanPham bangsanpham = db.BangSanPhams.Find(id);
+            if (bangsanpham == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bangsanpham);
+        }
+
+        // POST: /Create/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            BangSanPham bangsanpham = db.BangSanPhams.Find(id);
+            db.BangSanPhams.Remove(bangsanpham);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
